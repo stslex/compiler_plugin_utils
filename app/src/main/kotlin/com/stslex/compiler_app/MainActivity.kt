9610b8ef7de6e9e0910a1470647248c67f6b55d0
stslex <ilya977.077@gmail.com>
2025-02-14 18:37:06 +0300

    init application

--- app/src/main/kotlin/com/stslex/compiler_app/MainActivity.kt
+++ app/src/main/kotlin/com/stslex/compiler_app/MainActivity.kt
@@ -3,10 +3,10 @@ package com.stslex.compiler_app
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.stslex.compiler_app.UserToastUtil.sendToastOfUserChanges
 import com.stslex.compiler_app.app.R
 import com.stslex.compiler_app.model.UserModel
 import kotlinx.coroutines.flow.launchIn
@@ -36,25 +36,29 @@ class MainActivity : ComponentActivity() {
         findViewById<Button>(R.id.usernameChangeButton).setOnClickListener {
             logger.log(Level.INFO, "usernameChangeButton clicked")
             val randomInt = Random.nextInt()
-            viewModel.setName("John $randomInt")
+            viewModel.setName("Name $randomInt")
+        }
+        findViewById<Button>(R.id.secondNameChangeButton).setOnClickListener {
+            logger.log(Level.INFO, "secondNameChangeButton clicked")
+            val randomInt = Random.nextInt()
+            viewModel.setSecondName("SecondName $randomInt")
         }
     }
 
     private fun setUI(user: UserModel) {
         logger.log(Level.INFO, "Setting UI with user: $user")
         sendToastOfUserChanges(user)
-        findViewById<TextView>(R.id.usernameFieldTextView).text = user.name
+        setName(user.name)
+        setSecondName(user.secondName)
     }
 
-    private fun sendToastOfUserChanges(userModel: UserModel) {
-        val textMsg = userModel.getChangesValue(user)
-        Toast.makeText(this, textMsg, Toast.LENGTH_SHORT).show()
-        user = userModel
+    private fun setName(name: String) {
+        logger.log(Level.INFO, "setName: $name")
+        findViewById<TextView>(R.id.usernameFieldTextView).text = name
     }
 
-    companion object {
-
-        private var user: UserModel? = null
+    private fun setSecondName(name: String) {
+        logger.log(Level.INFO, "setSecondName: $name")
+        findViewById<TextView>(R.id.secondNameFieldTextView).text = name
     }
 }
-
--- app/src/main/kotlin/com/stslex/compiler_app/MainActivityViewModel.kt
+++ app/src/main/kotlin/com/stslex/compiler_app/MainActivityViewModel.kt
@@ -13,31 +13,12 @@ class MainActivityViewModel : ViewModel() {
     val userInfo: StateFlow<UserModel>
         get() = _userInfo.asStateFlow()
 
-    fun setIsLiked(isLiked: Boolean) {
-        _userInfo.value = _userInfo.value.copy(isLiked = isLiked)
-    }
-
-    fun setLikes(likes: Int) {
-        _userInfo.value = _userInfo.value.copy(likes = likes)
-    }
-
-    fun setSubscriptions(subscriptions: Int) {
-        _userInfo.value = _userInfo.value.copy(subscriptions = subscriptions)
-    }
-
-    fun setIsSubscribed(isSubscribed: Boolean) {
-        _userInfo.value = _userInfo.value.copy(isSubscribed = isSubscribed)
-    }
-
     fun setName(name: String) {
         _userInfo.value = _userInfo.value.copy(name = name)
     }
 
-    fun setAvatarUrl(avatarUrl: String) {
-        _userInfo.value = _userInfo.value.copy(avatarUrl = avatarUrl)
+    fun setSecondName(secondName: String) {
+        _userInfo.value = _userInfo.value.copy(secondName = secondName)
     }
 
-    fun setEmail(email: String) {
-        _userInfo.value = _userInfo.value.copy(email = email)
-    }
 }
